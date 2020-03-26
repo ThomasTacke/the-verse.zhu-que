de57978993e2f2a612bc314de2dc80b2ad63340c
Thomas Tacke <206163-RootTheKid@users.noreply.gitlab.com>
2020-03-26 18:46:29 +0100

    Added tado subscription. Reworked CSS a bit

--- src/app/service/mqtt-handler.service.ts
+++ src/app/service/mqtt-handler.service.ts
@@ -12,27 +12,36 @@ export class MqttHandlerService {
   kitchen: Room = new Room('Kitchen', []);
   floor: Room = new Room('Floor', []);
   livingRoom: Room = new Room('Living Room', []);
+  bathroom: Room = new Room('Bathroom', []);
 
   constructor(private mqttService: MqttService) {
     this.subscribeRoomFeatures();
   }
 
   private subscribeRoomFeatures(): void {
-    this.subscribeRoomFeature(this.kitchen, 'Temperature', 'the-verse/kitchen/temperature', Icons.temperatureIcon);
-    this.subscribeRoomFeature(this.kitchen, 'Humidity', 'the-verse/kitchen/humidity', Icons.humidityIcon);
+    this.subscribeRoomFeature(this.kitchen, 'Temperature NodeMCU', 'the-verse/kitchen/temperature', Icons.temperatureIcon);
+    this.subscribeRoomFeature(this.kitchen, 'Humidity NodeMCU', 'the-verse/kitchen/humidity', Icons.humidityIcon);
+    this.subscribeRoomFeature(this.kitchen, 'Temperature Tado', 'the-verse/kitchen/tado/temperature', Icons.temperatureIcon);
+    this.subscribeRoomFeature(this.kitchen, 'Humidity Tado', 'the-verse/kitchen/tado/humidity', Icons.humidityIcon);
     this.subscribeRoomFeature(this.kitchen, 'Light', 'the-verse/kitchen-pc/light', Icons.lightIcon);
+    this.subscribeRoomFeature(this.floor, 'Temperature NodeMCU', 'the-verse/floor/temperature', Icons.temperatureIcon);
+    this.subscribeRoomFeature(this.floor, 'Humidity NodeMCU', 'the-verse/floor/humidity', Icons.humidityIcon);
+    this.subscribeRoomFeature(this.livingRoom, 'Temperature Tado', 'the-verse/living-room/tado/temperature', Icons.temperatureIcon);
+    this.subscribeRoomFeature(this.livingRoom, 'Humidity Tado', 'the-verse/living-room/tado/humidity', Icons.humidityIcon);
+    this.subscribeRoomFeature(this.livingRoom, 'Vitrine Light', 'the-verse/vitrine/light', Icons.lightIcon);
+    this.subscribeRoomFeature(this.livingRoom, 'Nightstand Light', 'the-verse/nightstand/light', Icons.lightIcon);
+    this.subscribeRoomFeature(this.bathroom, 'Temperature Tado', 'the-verse/bathroom/tado/temperature', Icons.temperatureIcon);
+    this.subscribeRoomFeature(this.bathroom, 'Humidity Tado', 'the-verse/bathroom/tado/humidity', Icons.humidityIcon);
 
-    // this.kitchen.sensors.push(new Sensor('Fake', timer(1000, 5000).pipe(map(i => { return { payload: (i*2).toString() }}), shareReplay(2)) as Observable<IMqttMessage>, this.temperatureIcon));
+    // this.kitchen.sensors.push(new Sensor('Fake', timer(1000, 5000).pipe(map(i => {
+    //   return { payload: (i * 2).toString() }
+    // }),
+    //   shareReplay(2)) as Observable<IMqttMessage>, this.temperatureIcon));
 
     // this.kitchen.sensors.push(new Sensor('Fake', merge(
     //   of(null).pipe(mapTo({ payload: '1' }), delay(1000)),
     //   of(null).pipe(mapTo({ payload: '3' }), delay(5000)),
     // ).pipe(shareReplay(2)) as Observable<IMqttMessage>, this.temperatureIcon));
-
-    this.subscribeRoomFeature(this.floor, 'Temperature', 'the-verse/floor/temperature', Icons.temperatureIcon);
-    this.subscribeRoomFeature(this.floor, 'Humidity', 'the-verse/floor/humidity', Icons.humidityIcon);
-    this.subscribeRoomFeature(this.livingRoom, 'Vitrine Light', 'the-verse/vitrine/light', Icons.lightIcon);
-    this.subscribeRoomFeature(this.livingRoom, 'Nightstand Light', 'the-verse/nightstand/light', Icons.lightIcon);
   }
 
   private subscribeRoomFeature(room: Room, sensorName: string, topic: string, icon: string): void {
